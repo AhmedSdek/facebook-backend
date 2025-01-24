30a17a6477b602a900e8aa81e1ae3f0d634deb62
AhmedSdek <93591832+AhmedSdek@users.noreply.github.com>
2025-01-24 12:58:18 +0200

    ي

--- src/models/postModel.js
+++ src/models/postModel.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 const PostSchema = new mongoose.Schema({
     content: { type: String, required: true },
+    image: { type: String },
     createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, // علاقة بجدول المستخدمين
     createdAt: { type: Date, default: Date.now },
     userId: { type: String, required: true },
